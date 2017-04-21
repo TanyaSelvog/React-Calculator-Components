2a01bf5390f88d13be4e8a811c33349e266d5f3f
Tanya Selvog <tanyaselvog@gmail.com>
2017-04-20 20:19:42 -0500

    homework complete

---
+++ src/Box.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Box extends Component {
+  render() {
+    return (
+      <div className="box">
+        <div className="title">
+        <p>GA Instruments TI-WDI</p>
+      </div>)
+    }
+
+  export default Box; 
